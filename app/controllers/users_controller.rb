fce77dddf5b66f2c61fcb5940fbefd705dd9e305
tenmasato <narito1015@icloud.com>
2021-11-26 08:12:12 +0000

    [Add]削除機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @book = Book.new
+    @books = Book.all
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :books,only:[:index,:new,:create,:show]
+  resources :books
   resources :users
 end
