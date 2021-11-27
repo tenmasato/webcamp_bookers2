583188ed5eb3b10e830c4efc9b16e2db72869778
tenmasato <narito1015@icloud.com>
2021-11-27 07:21:09 +0000

    [Add]フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,8 +27,11 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book)
+    if @book.update(book_params)
+    redirect_to book_path(@book) , notice: "You have updated book successfully."
+    else
+    render "edit"
+    end
   end
 
   def destroy
